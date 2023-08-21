fa287ec3154191a3b69fcdf08bb96b98b7301b65
progIfe <ifeoluwasamson90@gmail.com>
2023-08-21 09:38:22 +0100

    Bug fix from dependencies

--- django_cache/settings.py
+++ django_cache/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
