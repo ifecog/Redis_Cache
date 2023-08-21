5e7353457f7d7700942c2a71d4a0335cebdedc94
progIfe <ifeoluwasamson90@gmail.com>
2023-08-21 13:18:00 +0100

    Product list and create endpoints

--- store/views.py
+++ store/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+from .models import Product
+from .serializers import ProductSerializer
+
 
 # Create your views here.
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)        
+    
+    def create(self, request):
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)        
+    
+    def retrieve(self, request, pk=None):
+        pass
+    
+    def update(self, request, pk=None):
+        pass
+    
+    def destroy(self, request, pk=None):
+        pass
+    
+      
