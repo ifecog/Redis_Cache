164d3275b6d3279cfd96fc3cbdc47b5709b15f2f
progIfe <ifeoluwasamson90@gmail.com>
2023-08-21 10:01:22 +0100

    Product model migrations

--- django_cache/settings.py
+++ django_cache/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'store.apps.StoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
